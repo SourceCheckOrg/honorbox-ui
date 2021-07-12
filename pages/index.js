5c21999dd3e6a69f030e6af9e4e8dcebf8e77f8a
By_caballero <37127325+bumblefudge@users.noreply.github.com>
2021-07-12 14:45:06 +0200

    to fix typos on index page

--- pages/index.js
+++ pages/index.js
@@ -21,26 +21,26 @@ export default function Home() {
   <h1>Welcome the HonorBox prototype</h1> \
   <p>\
     This prototype demonstrates how the HonorBox platform can be used to \
-    generate unique, per-publication revenue-sharing smart contracts and \ 
+    generate unique, per-publication revenue-sharing smart contracts and \
     attach those to PDF files for direct payments. \
     We even have code for detecting, extracting, a verifying signatures on \
     the W3C Verifiable credentials that describe all of these operations, \
     to make them more trustworthy.  Proposed future features include: \
     <ul> \
-      <li>Binding publisher identities to Twitter account control proofs, \ 
+      <li>Binding publisher identities to Twitter account control proofs, \
       long-lived blockchain addresses, or other proofs of authenticity</li> \
       <li>Support for more wallets and protocols for managing publications \
       and taking that data elsewhere in the form of Verifiable Credentials</li>\
       <li>Support for more blockchain address types for revenue-sharing contracts</li>\
       <li></li>\
-    </ul>\ 
+    </ul>\
   </p> \
   <p>\
     We currently use the <a href=https://spruceid.dev/docs/Credible/>Credible wallet</a> \
     from <a href=https://spruceid.com/>Spruce Systems</a>, USA, to handle \
     log-in to our custom, Strapi-powered publisher interface. Please \
     <strong>download</strong> the wallet from <a href=https://play.google.com/store/apps/details?id=com.spruceid.app.credible>Android store</a>\
-    or the iOS app store (coming soon) and use the "sign up" button above \
+    or the iOS app store (coming soon) and use the \"sign up\" button above \
     to get started.\
   </p>\
   \
